7fadda9e31d985d044d54cbc2e1defcbbd6395b1
Perry Hertler <perry@hertler.org>
2021-04-17 14:05:41 -0600

    style offenses gan

--- lib/google_subscriber.rb
+++ lib/google_subscriber.rb
@@ -1,14 +1,14 @@
-require "google_subscriber/version"
-require "google_subscriber/logging"
-require "google_subscriber/graceful_shutdown"
-require "google_subscriber/subscriber_registry"
-require "google_subscriber/base_subscriber"
+require 'google_subscriber/version'
+require 'google_subscriber/logging'
+require 'google_subscriber/graceful_shutdown'
+require 'google_subscriber/subscriber_registry'
+require 'google_subscriber/base_subscriber'
 
 module GoogleSubscriber
-  extend SubscriberRegistry, Logging
+  extend Logging
+  extend SubscriberRegistry
 
   def self.configure
     yield self if block_given?
   end
-
 end
